54829d45cd24516d373cf63c0631e51507909959
Sihvo Jouni <jouni.sihvo@cinia.fi>
2016-10-21 15:57:02 +0300

    modifications

--- client/ui/js/client.js
+++ client/ui/js/client.js
@@ -3,3 +3,11 @@
 'use strict';
 
 var app = angular.module('MTGOpenboard', ['ui.bootstrap']);
+//app.config(['$routeProvider',/*'$locationProvider',*/ function($routeProvider) {
+  //$routeProvider.when('/userInfo', { templateUrl: 'src/views/userInfo.html', controller: 'UserInfoViewController' });
+  //$routeProvider.when('/lobby', { templateUrl: 'src/views/lobby.html', controller: 'LobbyViewController' });
+  //$routeProvider.when('/deckCreator', { templateUrl: 'src/views/deckCreator.html', controller: 'DeckCreatorViewController' });
+  //$routeProvider.when('/logout', { redirectTo:'/' });
+  //$routeProvider.otherwise({redirectTo:'/'});
+  //$locationProvider.html5Mode(false);
+//}]);
