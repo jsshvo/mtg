572c2edcf8ff64dbc00417eae7998b56629a463a
Sihvo Jouni <jouni.sihvo@cinia.fi>
2016-10-26 17:45:24 +0300

    moved stuff

--- client/ui/js/client.js
+++ client/ui/js/client.js
@@ -9,19 +9,27 @@ angular.module('MTGOpenboard', ['ui.router', 'ui.bootstrap'/*, 'ngResource'*/]);
   }
 );*/
 angular.module('MTGOpenboard').config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-  //$urlRouterProvider.when('','/').otherwise('/');
-  /*$stateProvider.state('index', { url: '', controller: 'MainController', views: { 
-    'lobby': { templateUrl: 'templates/lobby.html', controller: 'LobbyController' },
-    'deckcreator': { templateUrl: 'templates/deck-creator.html', controller: 'DeckCreatorController' },
-    'board': { templateUrl: 'templates/board.html', controller: 'BoardController' }
-  }});*/
-  //$stateProvider.state('login', { url: '/login', templateUrl: '', controller: 'LoginController' });
-  //$stateProvider.state('error', { url: '/error', templateUrl: '', controller: 'ErrorController' });
-  //$locationProvider.html5Mode(true);
+  $urlRouterProvider.when('','/').otherwise('/');
+  $stateProvider.state('index', { url: '/', controller: 'MainController', views: { 
+    'lobby': { templateUrl: '/ui/templates/lobby.html', controller: 'LobbyController' },
+    'deckcreator': { templateUrl: '/ui/templates/deck-creator.html', controller: 'DeckCreatorController' },
+    'board': { templateUrl: '/ui/templates/board.html', controller: 'BoardController' }
+  }});
+  $stateProvider.state('login', { url: '/login', templateUrl: '', controller: 'LoginController' });
+  $stateProvider.state('error', { url: '/error', templateUrl: '', controller: 'ErrorController' });
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 }]);
 angular.module('MTGOpenboard').run(function($rootScope, $state/*, appConstants*/) {
-  //$state.go('index');
+  console.log("changing state to index");
+  $state.go('index');
+  console.log("done");
   $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-      console.log("STATE CHANGED FROM "+fromState+" TO "+toState);
+      console.log("STATE CHANGED FROM: ");
+      console.dir(fromState);
+      console.log(" TO: ");
+      console.dir(toState);
   });
 });
